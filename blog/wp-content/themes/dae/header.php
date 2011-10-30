e2113660a5e384f1832148bb2430b1ff1a44ca75
Kyota Yasuda <dasoran@gmail.com>
2011-10-31 00:37:07 +0900

    Aboutページ作成 CSSを分割 ブログの記事タイトルのデザイン変更 英語版ページをいくつか準備した。

--- blog/wp-content/themes/dae/header.php
+++ blog/wp-content/themes/dae/header.php
@@ -38,6 +38,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="../main.css" />
 <link rel="stylesheet" type="text/css" media="all" href="reset.css" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
